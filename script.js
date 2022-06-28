6792df9db9ecbd0ebb2ee2506af9609b4ee5df7a
Andy Tseng <anby1611@gmail.com>
2022-06-27 18:32:31 -0700

    Player Creation

--- script.js
+++ script.js
@@ -10,8 +10,20 @@ ctx.fillRect(0,0,canvas.width,canvas.height)
 const image = new Image()
 image.src = './Image/Pellet Town.png'
 
+const playerImage = new Image()
+playerImage.src = './Image/playerDown.png'
+
 image.onload = ()=>{
-    ctx.drawImage(image,-1400,-400)
+    ctx.drawImage(image,-1425,-490)
+    ctx.drawImage(
+        playerImage,
+        0, //crop from x 
+        0, //crop from y
+        playerImage.width/4, // crop width 
+        playerImage.height,// crop height
+        canvas.width/2- playerImage.width/8, //x
+        canvas.height/2 - playerImage.height/2, //y
+        playerImage.width/4, // actually width, scale rate
+        playerImage.height,// actually height,scale rate
+    )
 }
-
-
